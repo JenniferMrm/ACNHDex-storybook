ea885379cad25507222f858d2b733eaf6ed686d4
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-04-14 12:13:37 +0200

    :wrench: [config] : Add basic config for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../ui/components/**/*.stories.mdx",
+    "../ui/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../ui/segments/**/*.stories.mdx",
+    "../ui/segments/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue"
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@ui": path.resolve(__dirname, "../ui"),
+    };
+    return config;
+  },
+  framework: "@storybook/vue",
+};
