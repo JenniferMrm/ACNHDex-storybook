47153bff30a238b24db33caa88d096d41f793e1f
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-04-28 17:21:28 +0200

    :sparkles: [feat] : Add up-button component

---
+++ ui/components/buttons/up-button/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="up-button">
+    <div class="up-button__container">
+      <icon-chevron :size="18" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconChevron from "@ui/components/icons/icon-chevron";
+
+export default {
+  name: "UpButton",
+  components: {
+    IconChevron,
+  },
+};
+</script>
+
+<style></style>
---
+++ ui/components/icons/_icons-mixin.js
@@ -0,0 +1,12 @@
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "nc-00",
+    },
+    size: {
+      type: String,
+      default: "64",
+    },
+  },
+};
